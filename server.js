60573f2e848c8b7b79a2ff227c55edbb9e4a6bfc
Paul <pdsmart6@gmail.com>
2026-06-25 15:46:12 +0100

    change port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const typesRoutes = require('./routes/types');
 const { formatGBP, formatDate, displayDate } = require('./utils/format');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || '0.0.0.0';
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view cache', false);
